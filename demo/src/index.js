1f30f52d955184b902da1354fed2f35a445892dd
Richard <richard.dimalanta@gmail.com>
2018-06-07 21:52:33 +1000

    Initial commit of drift component.

---
+++ demo/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+import Drift from "react-driftjs";
+
+render(
+  <div>
+    <h2>React Drift Demo</h2>
+    <Drift appId="9h3gdx9mm6ze" />
+  </div>,
+  document.getElementById("app")
+);
