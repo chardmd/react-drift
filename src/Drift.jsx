468076462da52a3fff60e0b34a1c29c1a6743132
Richard Dimalanta <chardmd.au@gmail.com>
2018-09-29 09:08:17 +1000

    undefined check

--- src/Drift.jsx
+++ src/Drift.jsx
@@ -55,7 +55,7 @@ class Drift extends React.Component {
   }
 
   componentDidMount() {
-    if (typeof window !== "undefined") {
+    if (typeof window !== undefined) {
       this.addMainScript();
       this.addIdentityVariables();
     }
